aa000715e7b7fd80f04b9c91647974c49c20fb54
Eliana Henderson <elianalhenderson@gmail.com>
2023-02-06 14:50:05 -0800

    Added lateinit vars for app elements

--- app/src/main/java/com/example/pigdice/MainActivity.kt
+++ app/src/main/java/com/example/pigdice/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.pigdice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 const val RESET_SCORE_ON_ONES = true
 
 class MainActivity : AppCompatActivity() {
+
+    //You Variables
+    lateinit var tvYou : TextView
+    lateinit var tvYouGamesWon : TextView
+    lateinit var tvYouTotalScore : TextView
+    lateinit var tvYouTurnTotal : TextView
+
+    //Computer Variables
+    lateinit var tvComputer : TextView
+    lateinit var tvComputerGamesWon : TextView
+    lateinit var tvComputerTotalScore : TextView
+    lateinit var tvComputerTurnTotal : TextView
+
+    //Dice Variables
+    lateinit var tvScore : TextView
+    lateinit var ivDice1 : ImageView
+    lateinit var ivDice2 : ImageView
+
+    //Button Variables
+    lateinit var btnRoll : Button
+    lateinit var btnHold : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
